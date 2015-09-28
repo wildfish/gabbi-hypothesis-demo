f07332e575c9535d989ad4cbf3f5523e1486b709
Dan Bate <dan@wildfish.com>
2015-09-28 10:45:49 +0100

    added comments to the test case

--- app/test_case.py
+++ app/test_case.py
@@ -9,12 +9,18 @@ from six import StringIO
 
 
 class GabbiHypothesisTestCase(TestCase, LiveServerTestCase):
+    """
+    Test case to handle running gabbi tests along with hypothesis in django applications.
+    """
     def run_gabi(self, gabbi_declaration):
+        # initialise the gabbi handlers
         for handler in RESPONSE_HANDLERS:
             handler(HTTPTestCase)
 
+        # take only the host name and port from the live server
         _, host = self.live_server_url.split('://')
 
+        # use gabbi to create the test suite from our declaration
         suite = test_suite_from_yaml(
             unittest.defaultTestLoader,
             self.id(),
@@ -26,8 +32,11 @@ class GabbiHypothesisTestCase(TestCase, LiveServerTestCase):
             None,
         )
 
+        # run the test (we store the the output into a custom stream so that hypothesis can display only the simple
+        # case test result on failure rather than every failing case)
         s = StringIO()
         result = ConciseTestRunner(stream=s, verbosity=0).run(suite)
 
+        # if we weren't successfull we need to fail the test case with the error string from gabbi
         if not result.wasSuccessful():
             self.fail(s.getvalue())
